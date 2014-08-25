a8f75029a23eec3fb1c9c3ba697deddc3fd7e1e2
Mazetto <mazetto@kodame.com.br>
2014-08-25 09:04:42 -0300

    Separate functions calls

--- functions.php
+++ functions.php
@@ -2,9 +2,26 @@
 
 require_once STYLESHEETPATH . '/lib/root.php';
 
+// Apply actions & filters to admin and public area
 add_action( 'root_setup', 'custom_setup' );
 
+// Activities only in admin side
+add_action( 'root_admin_setup', 'admin_setup' );
+
+// Public interactions
+add_action( 'root_public_setup', 'public_setup' );
+
 function custom_setup()
+{
+
+}
+
+function custom_admin_setup()
+{
+
+}
+
+function custom_public_setup()
 {
 
 }
