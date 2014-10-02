893e23777d26c71926b7e17d7bff63df7f4e0ce6
Mazetto <mazetto@kodame.com.br>
2014-10-02 16:30:50 -0300

    Fixed, Callbacks' name in functions file

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@ require_once STYLESHEETPATH . '/lib/root.php';
 add_action( 'root_setup', 'custom_setup' );
 
 // Activities only in admin side
-add_action( 'root_admin_setup', 'admin_setup' );
+add_action( 'root_admin_setup', 'custom_admin_setup' );
 
 // Public interactions
-add_action( 'root_public_setup', 'public_setup' );
+add_action( 'root_public_setup', 'custom_public_setup' );
 
 function custom_setup()
 {
